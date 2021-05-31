9e7645ae8b8c651e8fdb90d023aa0ab280f26a6b
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-05-31 23:18:01 +0200

    products dynamic

---
+++ products.js
@@ -0,0 +1,34 @@
+fetch("https://keaprojects21-50cf.restdb.io/rest/ullo-products", {
+  method: "GET",
+  headers: {
+    "x-apikey": "602e38155ad3610fb5bb62bd",
+  },
+})
+  .then((res) => res.json())
+  .then((response) => {
+    console.log(response);
+    showProducts(response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+function showProducts(data) {
+  data.forEach((product) => {
+    console.log(product);
+
+    showProduct(product);
+  });
+}
+function showProduct(product) {
+  const tempProd = document.querySelector("template").content;
+  const clone = tempProd.cloneNode(true);
+
+  clone.querySelector(".product-name").textContent = product.name;
+  clone.querySelector(".product-image").src = product.image;
+
+  clone.querySelector(".product-price").textContent = `${product.price} kr`;
+
+  const prod = document.querySelector("#products");
+  prod.appendChild(clone);
+}
