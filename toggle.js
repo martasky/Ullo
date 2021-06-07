601bbaaace38c50d1dd23a1c84c6c0842fadde1d
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-06-07 20:03:38 +0200

    fix toggle

--- toggle.js
+++ toggle.js
@@ -1,29 +1,33 @@
-let lightOff = true
+let lightOff = true;
 
-toggle()
+toggle();
 
 function toggle() {
-if(lightOff == true) {
-document.querySelector("div .toggle-switch").addEventListener("click", switchOn);
+  if (lightOff == true) {
+    document
+      .querySelector("div .toggle-switch")
+      .addEventListener("click", switchOn);
 
-function switchOn() {
-document.querySelector(".title div").classList.remove("toggle-off");
-document.querySelector(".title div").classList.add("toggle")
-document.querySelector("#header-wrapper").classList.remove("lightoff");
-document.querySelector("#header-wrapper").classList.add("lighton")
-document.querySelector("#buynow").classList.add("glow");
-let lightOff = false
-document.querySelector("div .toggle-switch").addEventListener("click", switchOff);
+    function switchOn() {
+      document.querySelector(".title div").classList.remove("toggle-off");
+      document.querySelector(".title div").classList.add("toggle");
+      document.querySelector("#header-wrapper").classList.remove("lightoff");
+      document.querySelector("#header-wrapper").classList.add("lighton");
+      document.querySelector("nav .button").classList.add("glow");
+      let lightOff = false;
+      document
+        .querySelector("div .toggle-switch")
+        .addEventListener("click", switchOff);
 
-function switchOff() {
-  document.querySelector(".title div").classList.remove("toggle");
-  document.querySelector(".title div").classList.add("toggle-off");
-  document.querySelector("#header-wrapper").classList.remove("lighton");
-document.querySelector("#header-wrapper").classList.add("lightoff")
-document.querySelector("#buynow").classList.remove("glow");
-  let lightOff = true;
-  toggle();
-}
-}
-}
+      function switchOff() {
+        document.querySelector(".title div").classList.remove("toggle");
+        document.querySelector(".title div").classList.add("toggle-off");
+        document.querySelector("#header-wrapper").classList.remove("lighton");
+        document.querySelector("#header-wrapper").classList.add("lightoff");
+        document.querySelector("nav .button").classList.remove("glow");
+        let lightOff = true;
+        toggle();
+      }
+    }
+  }
 }
